b877682bbb0635ebc90ed06a2e3a05036030190f
WalkerAlone764 <jatinsinghal404@gmail.com>
2024-12-01 21:16:10 +0530

    feat: Initial setup
    - Added fonts (Manrope, Impact, Impacted) and their configurations.
    - Added a logo and an icon drawable.
    - Included a list of meme image resources.
    - Updated AndroidManifest to declare preloaded fonts.
    - Updated project JDK to 21.

---
+++ app/src/main/java/com/upsidedowndev/mastermeme/ui/theme/Font.kt
@@ -0,0 +1,22 @@
+package com.upsidedowndev.mastermeme.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.upsidedowndev.mastermeme.R
+
+val manropeFamily = FontFamily(
+    Font(R.font.manrope_light, FontWeight.Light),
+    Font(R.font.manrope, FontWeight.Normal),
+    Font(R.font.manrope_medium, FontWeight.Medium),
+    Font(R.font.manrope_semibold, FontWeight.SemiBold),
+    Font(R.font.manrope_bold, FontWeight.Bold)
+)
+
+val impact = FontFamily(
+    Font(R.font.impact, FontWeight.Normal)
+)
+
+val impacted = FontFamily(
+    Font(R.font.impacted, FontWeight.Normal)
+)
